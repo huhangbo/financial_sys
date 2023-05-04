b2172006e4d0cc180619f2da57ec1e97f1d1faf3
huhangbo.hhb <huhangbo.hhb@bytedance.com>
2023-05-04 17:43:21 +0800

    feat: 登录和注册

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,11 @@
 'use client';
-import React, { useState } from 'react';
-import { Form, Input, Button } from 'antd';
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Form, Input } from 'antd';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 const Login = () => {
+  const { replace } = useRouter();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
@@ -22,27 +25,39 @@ const Login = () => {
     setPassword(e.target.value);
   };
 
+  const toRegister = () => {
+    replace('/register');
+  };
+
   return (
-    <Form name="basic" onFinish={onFinish} onFinishFailed={onFinishFailed}>
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: 'Please input your username!' }]}
-      >
-        <Input value={username} onChange={handleUsernameChange} />
+    <Form
+      name="basic"
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+      style={{ maxWidth: 600, width: '75%', margin: '0 auto' }}
+    >
+      <Form.Item label="用户" name="username" rules={[{ required: true, message: '用户名不能为空' }]}>
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          value={username}
+          onChange={handleUsernameChange}
+        />
       </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password value={password} onChange={handlePasswordChange} />
+      <Form.Item label="密码" name="password" rules={[{ required: true, message: '密码不能为空' }]}>
+        <Input.Password
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          value={password}
+          onChange={handlePasswordChange}
+        />
       </Form.Item>
 
       <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
+        <Button type="primary" htmlType="submit" style={{ width: '100%', margin: '0 auto' }}>
+          登录
+        </Button>
+        <Button type="link" onClick={toRegister} style={{ width: '100%', margin: '0 auto' }}>
+          点击注册
         </Button>
       </Form.Item>
     </Form>
---
+++ src/app/register/page.tsx
@@ -0,0 +1,159 @@
+'use client';
+import { Button, Checkbox, Form, Input, Select } from 'antd';
+
+const { Option } = Select;
+
+interface DataNodeType {
+  value: string;
+  label: string;
+  children?: DataNodeType[];
+}
+
+const formItemLayout = {
+  labelCol: {
+    xs: { span: 24 },
+    sm: { span: 8 }
+  },
+  wrapperCol: {
+    xs: { span: 24 },
+    sm: { span: 16 }
+  }
+};
+
+const tailFormItemLayout = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0
+    },
+    sm: {
+      span: 16,
+      offset: 8
+    }
+  }
+};
+
+const Register = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
+
+  const prefixSelector = (
+    <Form.Item name="prefix" noStyle>
+      <Select style={{ width: 70 }}>
+        <Option value="86">+86</Option>
+      </Select>
+    </Form.Item>
+  );
+
+  return (
+    <Form
+      {...formItemLayout}
+      form={form}
+      name="register"
+      onFinish={onFinish}
+      initialValues={{ prefix: '86' }}
+      style={{ width: '75%', maxWidth: 600, margin: '0 auto' }}
+      scrollToFirstError
+    >
+      <Form.Item
+        name="nickname"
+        label="用户名"
+        tooltip="输入用户名用于标识登录"
+        rules={[{ required: true, message: 'Please input your nickname!', whitespace: true }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        name="password"
+        label="密码"
+        rules={[
+          {
+            required: true,
+            message: '请输入密码'
+          }
+        ]}
+        hasFeedback
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="confirm"
+        label="确认密码"
+        dependencies={['password']}
+        hasFeedback
+        rules={[
+          {
+            required: true,
+            message: '请再次输入密码'
+          },
+          ({ getFieldValue }) => ({
+            validator(_, value) {
+              if (!value || getFieldValue('password') === value) {
+                return Promise.resolve();
+              }
+              return Promise.reject(new Error('两次输入的密码不一样'));
+            }
+          })
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item name="phone" label="电话" rules={[{ required: true, message: '请输入电话' }]}>
+        <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
+      </Form.Item>
+
+      <Form.Item
+        name="email"
+        label="邮箱"
+        rules={[
+          {
+            type: 'email',
+            message: '邮箱格式不对'
+          },
+          {
+            required: true,
+            message: '请输入邮箱'
+          }
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item name="gender" label="性别" rules={[{ required: true, message: '请选择性别' }]}>
+        <Select placeholder="select your gender">
+          <Option value="male">男</Option>
+          <Option value="female">女</Option>
+        </Select>
+      </Form.Item>
+
+      <Form.Item
+        name="agreement"
+        valuePropName="checked"
+        rules={[
+          {
+            validator: (_, value) =>
+              value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement'))
+          }
+        ]}
+        {...tailFormItemLayout}
+      >
+        <Checkbox>
+          I have read the <a href="">agreement</a>
+        </Checkbox>
+      </Form.Item>
+      <Form.Item {...tailFormItemLayout}>
+        <Button type="primary" htmlType="submit" style={{ margin: '0 auto' }}>
+          注册
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+
+export default Register;
