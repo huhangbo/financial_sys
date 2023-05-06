1a9c3aeada662cefece3093ec1fa0653cafde85d
sunyiteng <sunyiteng@bytedance.com>
2023-05-07 03:32:28 +0800

    feat: ico and title, calculator

--- src/app/news/page.tsx
+++ src/app/news/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { Button } from 'antd';
+import Image from 'next/image';
 
 export default function hello() {
-  return <Button>11231</Button>;
+  return <Image src={'/news.png'} alt="news" width="1000" height="500" sizes="100vw" />;
 }
