59fa5f1dfe3c9f5225d2750c1d13071e5461c10f
sunyiteng <sunyiteng@bytedance.com>
2023-05-12 16:39:18 +0800

    fix: ssr config error

--- src/app/(launched)/layout.tsx
+++ src/app/(launched)/layout.tsx
@@ -1,5 +1,10 @@
 'use client';
-import RootLayout from './rootLayout';
+import dynamic from 'next/dynamic';
+
+const RootLayout = dynamic(()=>{
+  return import('./rootLayout');
+}, {ssr:false})
+
 function Layout({ children }: any) {
   return <RootLayout>{children}</RootLayout>;
 }
